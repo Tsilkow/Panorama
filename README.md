3c6d98229a567d09b1596bf35f000395cc23479e
Tom <tmsilkowski@gmail.com>
2022-11-27 15:51:01 +0100

    Rewritting of camera_calibration.py to save coefficients in a file

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Panorama
 
 This project is for an assignment for Robot Control course.
-Goal is to capture an two images using a provided camera, undistort them (using calibration data qcquired in a previous excersize, however the code needed to do so is included here) and stitch them together to create panorama
+Goal is to capture an two images using a provided camera, undistort them (using calibration data acquired in a previous excersize, however the code needed to do so is included here) and stitch them together to create panorama.  
--- camera_calibration.py
+++ camera_calibration.py
@@ -1,10 +1,21 @@
 import cv2
 import numpy as np
 import os
+import sys
 
-dir = 'img'
-cal_prefix = 'c_'
-val_prefix = 'v_'
+src_dir = 'img_src'
+out_file = 'cal_coeffs'
+
+# Scripted designed to calibrate basic distortions of a camera, using a series
+# of images (works best with >20) containing a flat laying checker board
+# pattern (with 6 by 9 squares) in captured at different perspectives.
+#
+# Images should be saved in img_src and have the same resolution.
+#
+# Calibration coefficients are stored in cal_coeffs.
+#
+# Additionally, undistort function is provided.
+
 
 def checker_points(width: int, size: tuple):
     assert len(size) == 2
@@ -12,47 +23,61 @@ def checker_points(width: int, size: tuple):
     result[:, :2] = width * np.mgrid[0:size[0], 0:size[1]].T.reshape(-1, 2)
     return result
 
+
 def calibrate(filenames):
     obj_points = []
     img_points = []
     pattern_size = (5, 8)
     tmplt_obj = checker_points(30, pattern_size)
     for filename in filenames:
-        img = np.array(cv2.imread(filename))
+        img = np.array(cv2.imread(src_dir+'/'+filename))
         img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        #cv2.imshow("demo", img)
-        #cv2.waitKey(100)
+        # cv2.imshow("demo", img)
+        # cv2.waitKey(100)
         corners = None
         result, corners = cv2.findChessboardCorners(img, pattern_size, corners)
         print(f'{filename}: {result}')
         if result:
-            corners = cv2.cornerSubPix(img, corners, (11, 11), (-1, -1), criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001))
+            corners = cv2.cornerSubPix(img, corners, (11, 11), (-1, -1),
+                                       criteria=(cv2.TERM_CRITERIA_EPS +
+                                                 cv2.TERM_CRITERIA_MAX_ITER,
+                                                 30, 0.001))
             img_points.append(corners)
             obj_points.append(tmplt_obj)
-            cv2.drawChessboardCorners(img, pattern_size, corners, True)
-            #cv2.imwrite(f'out_{filename}', img)
 
-    ret, camera_matrix, dist_coeffs, rvecs, tvecs = cv2.calibrateCamera(obj_points, img_points, img.shape[::-1], None, None)
+    ret, camera_matrix, dist_coeffs, rvecs, tvecs = \
+        cv2.calibrateCamera(obj_points, img_points, img.shape[::-1], None,
+                            None)
 
     return camera_matrix, dist_coeffs
 
-def undistort(filenames, camera_matrix, dist_coeffs):
+
+def undistort(filenames, src_dir, out_dir, camera_matrix, dist_coeffs):
     for filename in filenames:
-        img = np.array(cv2.imread(filename))
+        img = np.array(cv2.imread(src_dir+'/'+filename))
         img = cv2.undistort(img, camera_matrix, dist_coeffs)
-        print(f'out_{filename}')
-        cv2.imwrite(f'out_{filename}', img)
+        print(out_dir+'/'+filename)
+        cv2.imwrite(out_dir+'/'+filename, img)
+
 
 def main():
     cv2.namedWindow("demo")
-    calibration_imgs = [f'{dir}/{f}' for f in os.listdir(dir) if cal_prefix in f]
-    validation_imgs = [f'{dir}/{f}' for f in os.listdir(dir) if val_prefix in f]
+    calibration_imgs = [f for f in os.listdir(src_dir) if f[-4:] == '.png']
     assert len(calibration_imgs) != 0
-    assert len(validation_imgs) != 0
     camera_matrix, dist_coeffs = calibrate(calibration_imgs)
-    print(f'Camera Matrix = \n{camera_matrix}')
-    print(f'Distortion Coefficients = \n{dist_coeffs}')
-    undistort(validation_imgs, camera_matrix, dist_coeffs)
+
+    # Uncomment this to undistort images used for calibration
+    # undistort(calibration_imgs, src_dir, 'img_cal', camera_matrix, dist_coeffs)
+
+    print(camera_matrix, dist_coeffs, sep='\n')
+    og_stdout = sys.stdout
+    try:
+        f = open(out_file, 'w')
+        sys.stdout = f
+        print(camera_matrix, dist_coeffs, sep='\n')
+    finally:
+        sys.stdout = og_stdout
+
 
 if __name__ == '__main__':
     main()
